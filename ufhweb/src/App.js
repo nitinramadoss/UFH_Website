bf59ef2b88563ef38e34dd7122c7766e2091eec3
nitinramadoss <nitinramadoss@gmail.com>
2021-04-11 13:41:59 -0400

    Updates background and adds scroll plugin

--- ufhweb/src/App.js
+++ ufhweb/src/App.js
@@ -3,6 +3,7 @@ import React, { Component, useEffect } from 'react';
 import { Navbar } from 'react-bootstrap';
 import Carousel from "react-elastic-carousel";
 import Item from "./Item";
+import ReactPlayer from 'react-player/youtube'
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -12,8 +13,8 @@ const breakPoints = [
 ];
 
 function App() { 
-  const [offsetY, setOffsetY]= React.useState(0);
-  const handleScroll = () => setOffsetY(window.pageYOffset);
+  const [offset, setOffset]= React.useState(0);
+  const handleScroll = () => setOffset(window.pageYOffset);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -25,30 +26,74 @@ function App() {
 
   return (
     <div className="App">
-      <div className = "home">
-      {/* <Navbar className = "nav" sticky="top">
-        <Navbar.Brand>UF Hackathoners</Navbar.Brand>
-      </Navbar> */}
-        
-      <div className = "title">
-        <h1>UF Hackathoners</h1>  
-      </div>    
 
-      </div>
+      <section>
+        <div className = "home">
+          <h1>
+            UF Hackathoners <br/>
+          </h1>
+          <p>An ACM Special Interest Group</p>
+        </div>
+      </section>
 
-      <div className = "projects">        
-        <Carousel breakPoints={breakPoints} style={ {transform: `translateY(${offsetY*0.5}px)`}}>
-          <Item>One</Item>
-          <Item>Two</Item>
-          <Item>Three</Item>
-          <Item>Four</Item>
-          <Item>Five</Item>
-          <Item>Six</Item>
-          <Item>Seven</Item>
-          <Item>Eight</Item>
-        </Carousel>   
-      
-      </div>
+      <section>
+        <div className = "projects">        
+          <Carousel breakPoints={breakPoints}>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=0Hh8VspdDWs&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=osYg9M86WUU&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=vHYlUago1nU&feature=emb_imp_woyt' /> 
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=kUTqOXU9ypo&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=AgzC_uuOSPg&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=rgMyuhUWhz8&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=WWbYugJHh28&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=QsaD0D6Kwe0&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=kjyfBYvqjAc&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=sGpmK0HYS6s&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=qjPpTPBk1YU&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=TCdYTP8y7V0' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=lfjNs9mp4sM&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=Xxa5DlaqBBg&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=n6VCUdlhExs&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=ekmyuBPPni0&feature=emb_imp_woyt' />
+            </Item>
+            <Item>
+              <ReactPlayer url='https://www.youtube.com/watch?v=ekmyuBPPni0&feature=emb_imp_woyt' />
+            </Item>
+          </Carousel>          
+        </div>
+      </section>
+
     </div>
   ); 
 }
