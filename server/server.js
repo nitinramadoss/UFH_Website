060e3a1e595a4b84b8ecfcf121f316fecbb0ce76
nitinramadoss <nitinramadoss@gmail.com>
2021-04-18 10:38:10 -0400

    adds mongo client library to server file and queries data on express routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const { MongoClient } = require("mongodb");
 const app = express()
 const port = 8000
 
+const uri =
+  "mongodb+srv://nitinramadoss:ufh123@cluster0.u5z4w.mongodb.net/UFH_Website?retryWrites=true&writeConcern=majority";
+
+const client = new MongoClient(uri);
+
 app.use(express.json()); 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+	let user = await queryDB();
+	res.json({
+		name : user.name,
+		hackathons: user.hackathons,
+		wins: user.wins,
+		points: user.MLH_points
+	})
+})
+
+app.get('/loadLeaderboard', (req, res) => {
+	res.send('Hello World!')
 })
 
 app.post('/sendData', function (req, res) {
@@ -16,3 +32,26 @@ app.post('/sendData', function (req, res) {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+async function queryDB() {
+	let document = null;
+
+	try {
+	  await client.connect();
+  
+	  const database = client.db('leaderboard');
+	  const points = database.collection('member_points');
+  
+	  const query = { name : "Aditya Nair"};
+	  document = await points.findOne(query);
+
+	  console.log(document);
+  
+	  return document;
+	} finally {
+	  // Ensures that the client will close when you finish/error
+	  await client.close();
+	}
+
+	return null;
+}
